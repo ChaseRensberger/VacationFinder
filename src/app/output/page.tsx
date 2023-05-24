ba798ea773fe8f96948633c934a53243eb9f05e6
ChaseRensberger <chasehrensberger@gmail.com>
2023-05-24 16:58:32 -0400

    add fetch and loading page

--- src/app/output/page.tsx
+++ src/app/output/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 import { useForm } from "@/components/context/FormProvider";
+
+const getFormOutput = async () => {
+	const res = await fetch("/api/vacation", {
+		method: "GET",
+	});
+	if (!res.ok) {
+		throw new Error("Failed to fetch data");
+	}
+
+	return res.json;
+};
+
 const Output = async () => {
 	const { formData } = useForm();
+	const vacationResponse = await getFormOutput();
+
 	return (
 		<main className="flex min-h-screen flex-col items-center p-24 gap-16">
-			<button onClick={() => console.log(formData)}>CLICK ME</button>
+			<button onClick={() => console.log(vacationResponse)}>CLICK ME</button>
 		</main>
 	);
 };
