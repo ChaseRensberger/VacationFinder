6dceae24bd1982139e2eede0ca7e2f3f7ca46ca2
ChaseRensberger <chasehrensberger@gmail.com>
2023-05-24 22:41:20 -0400

    update gitignore

--- src/app/output/page.tsx
+++ src/app/output/page.tsx
@@ -8,10 +8,13 @@ import {
 } from "@/lib/config";
 
 const getFormOutput = async (formInput: string) => {
-	const formOutput = await fetch("../api/vacation", {
-		method: "POST",
-		body: formInput,
-	}).then((res) => {
+	const formOutput = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_URL}/api/vacation`,
+		{
+			method: "POST",
+			body: formInput,
+		}
+	).then((res) => {
 		if (!res.ok) {
 			throw new Error("Failed to fetch data");
 		} else {
